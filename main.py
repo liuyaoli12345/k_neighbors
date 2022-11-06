afd5d9969e6ea4c713689c1a4dc1224bca6910da
liuyaoli12345 <linshuoming12345@163.com>
2022-11-07 00:22:50 +0800

    i always spell the wrong variables...

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import numpy as np
+import operator
+
+def classify0(inX, dataSet, labels, k):
+    dataSetSize = dataSet.shape[0]
+    diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
+    sqDiffMat = diffMat**2
+    sqDistances = sqDiffMat.sum(axis=1)
+    distances = sqDistances**0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel,0) + 1
+        sortedClassCount = sorted(
+                classCount.items(),key=operator.itemgetter(1),reverse=True)
+        return sortedClassCount[0][0]
 
 def createDataSet():
     group = np.array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
@@ -22,4 +38,4 @@ if __name__ == '__main__':
     print('labels', labels)
     #test img2vector
     img2vector('digits/testDigits/0_1.txt')
-
+    print(classify0([0,0],group,labels,3))
