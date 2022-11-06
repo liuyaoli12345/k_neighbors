659d86c4f3a677b39c3d680c70bec88ed38aee50
liuyaoli12345 <linshuoming12345@163.com>
2022-11-07 00:02:09 +0800

    first try writing code with command line

--- main.py
+++ main.py
@@ -5,9 +5,21 @@ def createDataSet():
     labels = ['A', 'A', 'B', 'B']
     return group,labels
 
+def img2vector(filename):
+    returnVect = np.zeros((1,1024))
+
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0,32*i+j] = int(lineStr[j])
+        return returnVect
+
 if __name__ == '__main__':
     group,labels = createDataSet()
 
     print('group', group)
     print('labels', labels)
+    #test img2vector
+    img2vector('digits/testDigits/0_1.txt')
 
